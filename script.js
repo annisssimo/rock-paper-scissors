9c6b710d18605d3a1a0abb6d12f34c8f1182fdc8
annisssimo <anyalar04@gmail.com>
2023-11-05 19:15:14 +0300

    feat: add a getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert(Number(Math.random()));
+function getComputerChoice () {
+    let rock = 'rock';
+    let paper = 'paper';
+    let scissors = 'scissors';
+
+    let computerSelection = Math.floor(Math.random()*3);
+
+    switch (computerSelection) {
+        case 0: {return 'rock'};
+        break;
+        case 1: {return 'paper'};
+        break;
+        case 2: {return 'scissors'};
+    }
+}
+
+
+
